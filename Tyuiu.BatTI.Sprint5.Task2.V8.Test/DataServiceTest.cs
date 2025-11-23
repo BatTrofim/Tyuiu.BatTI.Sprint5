131091b88b54de507981b9e2546aa57e09457a47
Trofim Bat <Tyuiu.BatTI@outlook.com>
2025-11-23 15:04:52 +0500

    Task2

---
+++ Tyuiu.BatTI.Sprint5.Task2.V8.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.BatTI.Sprint5.Task2.V8.Lib;
+
+
+namespace Tyuiu.BatTI.Sprint5.Task2.V8.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string path = @"C:\Users\Lenovo\source\repos\Tyuiu.BatTI.Sprint5\Tyuiu.BatTI.Sprint5.Task2.V8\bin\Debug\OutPutFileTask2.csv";
+            FileInfo fileInfo = new FileInfo(path);
+            Assert.IsTrue(fileInfo.Exists);
+        }
+    }
+}
---
+++ Tyuiu.BatTI.Sprint5.Task2.V8/Program.cs
@@ -0,0 +1,42 @@
+﻿using Tyuiu.BatTI.Sprint5.Task2.V8.Lib;
+DataService ds = new DataService();
+
+
+Console.Title = "Спринт #5 | Выполнил: Бат Т. И. | ПИНб-25-1";
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* Спринт #5                                                               *");
+Console.WriteLine("* Тема: Класс File                                                        *");
+Console.WriteLine("* Задание #2                                                              *");
+Console.WriteLine("* Вариант #8                                                              *");
+Console.WriteLine("* Выполнил: Бат Трофим Иванович  | ПИНб-25-1                              *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+Console.WriteLine("***************************************************************************");
+
+int[,] matrix = { { -9, 4, -6 }, { 3, 9, 9 }, { -6, 7, 4 } };
+for (int i = 0; i < 3; i++)
+{
+    for (int j = 0, count = 0; j < 3; j++, count++)
+    {
+        if (count != 2)
+        {
+            Console.Write(matrix[i, j] + "\t");
+        }
+        else
+        {
+            Console.Write(matrix[i, j] + "\n");
+        }
+    }
+}
+Console.WriteLine();
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+Console.WriteLine("***************************************************************************");
+
+string path = ds.SaveToFileTextData(matrix);
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine(File.ReadAllText(path));
+Console.WriteLine("***************************************************************************");
+Console.ReadKey();
