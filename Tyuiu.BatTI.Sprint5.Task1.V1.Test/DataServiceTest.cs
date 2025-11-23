3209c18b027c855887ba50b75e025310f7d206ff
Trofim Bat <Tyuiu.BatTI@outlook.com>
2025-11-23 14:37:00 +0500

    Task1

---
+++ Tyuiu.BatTI.Sprint5.Task1.V1.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Tyuiu.BatTI.Sprint5.Task1.V1.Lib;
+
+namespace Tyuiu.BatTI.Sprint5.Task1.V1.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string path = @"C:\Users\Lenovo\source\repos\Tyuiu.BatTI.Sprint5\Tyuiu.BatTI.Sprint5.Task1.V1\bin\Debug\OutPutFileTask1.txt";
+
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExsists = fileInfo.Exists;
+            bool wait = true;
+            Assert.AreEqual(wait, fileExsists);
+        }
+    }
+}
---
+++ Tyuiu.BatTI.Sprint5.Task1.V1/Program.cs
@@ -0,0 +1,28 @@
+﻿using Tyuiu.BatTI.Sprint5.Task1.V1.Lib;
+DataService ds = new DataService();
+
+
+Console.Title = "Спринт #5 | Выполнил: Бат Т. И. | ПИНб-25-1";
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* Спринт #5                                                               *");
+Console.WriteLine("* Тема: Класс File                                                        *");
+Console.WriteLine("* Задание #1                                                              *");
+Console.WriteLine("* Вариант #1                                                              *");
+Console.WriteLine("* Выполнил: Бат Трофим Иванович  | ПИНб-25-1                              *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+Console.WriteLine("***************************************************************************");
+
+int startValue = -5;
+int stopValue = 5;
+Console.WriteLine("startValue =  " + startValue);
+Console.WriteLine("stopValue =  " + stopValue);
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+Console.WriteLine("***************************************************************************");
+
+string res = ds.SaveToFileTextData(startValue, stopValue);
+Console.WriteLine(res);
+Console.ReadKey();
