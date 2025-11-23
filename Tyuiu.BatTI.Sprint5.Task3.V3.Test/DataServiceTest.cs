93f25f03441b438d5d3bc4de4b7f2b8320b5ceb2
Trofim Bat <Tyuiu.BatTI@outlook.com>
2025-11-23 20:53:58 +0500

    Task3

--- Tyuiu.BatTI.Sprint5.Task3.V3.Test/DataServiceTest.cs
+++ Tyuiu.BatTI.Sprint5.Task3.V3.Test/DataServiceTest.cs
@@ -10,12 +10,12 @@ namespace Tyuiu.BatTI.Sprint5.Task3.V3.Test
         [TestMethod]
         public void TestMethod1()
         {
-            string path = @"C:\\Users\\Lenovo\\source\\repos\\Tyuiu.BatTI.Sprint5\\Tyuiu.BatTI.Sprint5.Task3.V3\\bin\\Debug\\OutPutFileTask3.bin";
+            string path = @"C:\Users\Lenovo\source\repos\Tyuiu.BatTI.Sprint5\Tyuiu.BatTI.Sprint5.Task3.V3\bin\Debug\OutPutFileTask3.bin";
 
-            FileInfo fileinfo = new FileInfo(path);
-            bool fileExits = fileinfo.Exists;
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExists = fileInfo.Exists;
             bool wait = true;
-            Assert.AreEqual(wait, fileExits);
+            Assert.AreEqual(wait, fileExists);
         }
     }
 }
