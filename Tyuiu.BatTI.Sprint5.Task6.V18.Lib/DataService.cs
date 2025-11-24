2a9647c5988ad80407ab8e9608c0d83bac03b874
Trofim Bat <Tyuiu.BatTI@outlook.com>
2025-11-24 20:42:21 +0500

    Task6

---
+++ Tyuiu.BatTI.Sprint5.Task6.V18.Lib/DataService.cs
@@ -0,0 +1,42 @@
+﻿using System.IO;
+using tyuiu.cources.programming.interfaces.Sprint5;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Tyuiu.BatTI.Sprint5.Task6.V18.Lib
+{
+    public class DataService : ISprint5Task6V18
+    {
+        public int LoadFromDataFile(string path)
+        {
+            int count = 0;
+            using (StreamReader reader = new StreamReader(path))
+            {
+                string line;
+                string s = "0123456789";
+
+                while ((line = reader.ReadLine()) != null)
+                {
+                    for (int i = 0; i < line.Length - 2; i++)
+                    {
+                        string str0 = line[i].ToString();
+                        string str = line[i + 1].ToString();
+                        string str1 = line[i + 2].ToString();
+
+                        if (i == 0)
+                        {
+                            if ((s.Contains(str0)) & !(s.Contains(str)))
+                            {
+                                count++;
+                            }
+                        }
+                        if (!(s.Contains(str0)) & s.Contains(str) & !(s.Contains(str1)))
+                        {
+                            count++;
+                        }
+                    }
+                }
+            }
+            return count;
+        }
+    }
+}
---
+++ Tyuiu.BatTI.Sprint5.Task6.V18/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.IO;
+using Tyuiu.BatTI.Sprint5.Task6.V18.Lib;
+DataService ds = new DataService();
+
+
+Console.Title = "Спринт #5 | Выполнил: Бат Т. И. | ПИНб-25-1";
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* Спринт #5                                                               *");
+Console.WriteLine("* Тема: Класс File                                                        *");
+Console.WriteLine("* Задание #6                                                              *");
+Console.WriteLine("* Вариант #18                                                             *");
+Console.WriteLine("* Выполнил: Бат Трофим Иванович  | ПИНб-25-1                              *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+Console.WriteLine("***************************************************************************");
+
+string path = @"C:\DataSprint5\InPutDataFileTask6V18.txt";
+
+Console.WriteLine("Данные находятся в файле: " + path);
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+Console.WriteLine("***************************************************************************");
+
+double res = ds.LoadFromDataFile(path);
+Console.WriteLine(res);
+Console.ReadKey();
