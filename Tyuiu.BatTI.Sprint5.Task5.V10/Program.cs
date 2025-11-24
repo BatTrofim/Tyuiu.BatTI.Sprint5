09811183f8093146a25f84189f1264c9ce69f5df
Trofim Bat <Tyuiu.BatTI@outlook.com>
2025-11-24 17:55:52 +0500

    Task5

---
+++ Tyuiu.BatTI.Sprint5.Task5.V10/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.IO;
+using Tyuiu.BatTI.Sprint5.Task5.V10.Lib;
+DataService ds = new DataService();
+
+
+Console.Title = "Спринт #5 | Выполнил: Бат Т. И. | ПИНб-25-1";
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* Спринт #5                                                               *");
+Console.WriteLine("* Тема: Класс File                                                        *");
+Console.WriteLine("* Задание #5                                                              *");
+Console.WriteLine("* Вариант #10                                                             *");
+Console.WriteLine("* Выполнил: Бат Трофим Иванович  | ПИНб-25-1                              *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+Console.WriteLine("***************************************************************************");
+
+string path = @"C:\DataSprint5\InPutDataFileTask5V10.txt";
+
+Console.WriteLine("Данные находятся в файле: " + path);
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+Console.WriteLine("***************************************************************************");
+
+double res = ds.LoadFromDataFile(path);
+Console.WriteLine(res);
+Console.ReadKey();
